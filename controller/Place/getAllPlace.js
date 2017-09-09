8f7c54ae0c034f1a58c6e68e60361f822c51a968
Yu Yongwoo <uyu423@gmail.com>
2017-09-09 21:50:54 +0900

    Place 정보에 reward Product 정보도 같이 주도록 수정

--- controller/Place/getAllPlace.js
+++ controller/Place/getAllPlace.js
@@ -3,15 +3,26 @@ const Service = require('../../service');
 
 module.exports = async function getAllPlace({ userId }) {
   const places = await Service.Place.getAll();
+  const products = await Service.Product.getAll();
+  const placeProductRewards = await Service.RelationPlaceProductReward.getAll();
+
+  const associatePlaces = places.map((place) => {
+    const supplyPlace = placeProductRewards.filter(relation => relation.placeId === place.id);
+    const productIds = supplyPlace.map(relation => relation.productId);
+
+    return Object.assign(place.get(), {
+      products: products.filter(product => productIds.indexOf(product.id) >= 0),
+    });
+  });
   if (userId) {
     const relations = await Service.RelationUserPlaceConquest.getRelationByUserId(userId);
-    places.map((place) => {
+    associatePlaces.map((place) => {
       const relation = _.find(relations, { placeId: place.id });
       if (relation) {
-        return Object.assign(place.get(), { conquest: true });
+        return Object.assign(place, { conquest: true });
       }
-      return Object.assign(place.get(), { conquest: false });
+      return Object.assign(place, { conquest: false });
     });
   }
-  return places;
+  return associatePlaces;
 };
